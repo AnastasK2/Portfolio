f413e1825b87158106ac05802f640e13a51c1897
AnastasK2 <zaporozhetsanastasia12@gmail.com>
2020-08-21 16:06:42 +0300

    Доделала адаптив, добавила якорь на кнопку

--- js/main.js
+++ js/main.js
@@ -8,3 +8,12 @@ var mySwiper = new Swiper('.swiper-container', {
         clickable: true
     },
 });
+
+$(document).ready(function(){
+    $("#yak").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
